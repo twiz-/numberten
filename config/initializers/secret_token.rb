0ffe41986b93160e228da5eb09c91fb2843ea0b4
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-12-07 12:57:22 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numberten::Application.config.secret_token = '19045ecdd68ac907d66718e7c21bfe4d1e8b786ef8af5f97c481f075840dc2a389f51d8de39f16f3596e8a0bbe4e327beb0896108912771b7de487f033b3bb1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Numberten::Application.config.session_store :cookie_store, key: '_numberten_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Numberten::Application.config.session_store :active_record_store
